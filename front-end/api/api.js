0a091b7f64e0bb15866aceffa881f7ff7bb16825
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2025-02-27 23:44:44 -0300

    configurada URL do deploy render para front-end fazer requisicao get nela, ajuste no process.env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,12 +2,14 @@
 import axios from "axios";
 
 //vai acessar o valor da variavel de ambient .env
-import "dotenv/config"
+//import "dotenv/config" // tem que apagar as variaveis de ambiente pois tava dando erro
 
 //const NODE_ENV = process.env.NODE_ENV
-const {NODE_ENV} = process.env
+// const {NODE_ENV} = process.env
 
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api": "/api"
+// const URL = NODE_ENV === 'development' ? "http://localhost:3000/api": "/api"
+
+const URL = "https://projeto-14-full-stack.onrender.com/api"
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
